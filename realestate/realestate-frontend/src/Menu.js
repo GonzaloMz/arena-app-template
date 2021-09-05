50238c978b3c938c72b6c0deff5a5b0beee0ffb4
Gonzalo Martinez <gonzalo@botbit.io>
2021-09-05 18:53:49 -0300

    mejoras en el menu

--- realestate/realestate-frontend/src/Menu.js
+++ realestate/realestate-frontend/src/Menu.js
@@ -13,41 +13,41 @@ const Menu = ({options}) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light navbar navbar-default navbar-fixed-top" style={{fontFamily: 'Roboto Cn'}}>
             <div className='container'>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand" href="https://costoya.com.ar/"><img style={{width:'80px'}} src='/LogoCostoya.svg'></img></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <a className="navbar-brand m-auto" href="https://costoya.com.ar/"><img style={{width:'80px'}} src='/LogoCostoya.svg'></img></a>
 
-            {   !showLogin &&
-                <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto text-left mt-3" >
-                        {
-                            options.map((option, i)=>{
-                                return(
-                                    <li data-toggle="collapse" data-target="#navbarSupportedContent" key={i} className="nav-item">
-                                        <Link   className="nav-link"  to={option.href}>{option.label}</Link>
-                                    </li>
-                                )
-                            })
+                {   !showLogin &&
+                    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                        <ul className="navbar-nav text-left m-auto" >
+                            {
+                                options.map((option, i)=>{
+                                    return(
+                                        <li data-toggle="collapse" data-target="#navbarSupportedContent" key={i} className="nav-item">
+                                            <Link   className="nav-link"  to={option.href}>{option.label}</Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                }
+                {
+                    !showLogin &&
+                    <a className="logout my-auto" href="/"
+                        onClick={
+                            ()=>{
+                                localStorage.removeItem('arena-authorization'); 
+                                localStorage.removeItem('arena-administrator'); 
+                            }
                         }
-                    </ul>
-                </div>
-            }
-            {
-                !showLogin &&
-                <a className="navbar-brand logout" href="/"
-                    onClick={
-                        ()=>{
-                            localStorage.removeItem('arena-authorization'); 
-                            localStorage.removeItem('arena-administrator'); 
-                        }
-                    }
-                ><FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon></a>
-            }
-            {
-                showLogin &&
-                <a className="navbar-brand login" href="/build/container/create/logIn">Ingresar</a>
-            }
+                    ><FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon></a>
+                }
+                {
+                    showLogin &&
+                    <a className="login my-auto" href="/build/container/create/logIn">Ingresar</a>
+                }
             </div>
             
         </nav>)
